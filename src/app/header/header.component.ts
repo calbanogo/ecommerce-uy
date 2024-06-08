af94c7d11dd05db93f1e6f2b8e2cb5e2c4ae6083
Christian Albano <calbanogo@gmail.com>
2024-06-08 22:40:18 +0200

    feat: añadir componete header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+          label: 'Home',
+          icon: 'pi pi-home'
+      },
+      {
+          label: 'Features',
+          icon: 'pi pi-star'
+      },
+      {
+          label: 'Projects',
+          icon: 'pi pi-search',
+      },
+      {
+          label: 'Ayuda',
+          icon: 'pi pi-envelope',
+          // badge: '3'
+      }
+  ];
+}
+}
